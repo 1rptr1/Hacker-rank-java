d2d8ccb4d9a90d1c3494e91a1173b1294bea619f
saurav <sauravbhr712@gmail.com>
2023-01-22 20:58:16 +0530

    done

--- src/main/java/SeparatetheNumbers.java
+++ src/main/java/SeparatetheNumbers.java
@@ -10,15 +10,49 @@ public class SeparatetheNumbers {
 
         for (int qItr = 0; qItr < q; qItr++) {
             String s = bufferedReader.readLine();
-
             SeparatetheNumbers.separateNumbers(s);
         }
 
         bufferedReader.close();
     }
     public static void separateNumbers(String s) {
-        
-        
-    
+        String flag = "NO";
+        int ans1 = 0;
+        if(s.length()>1){
+            int j= Integer.valueOf(s.substring(0, 2));
+            String number = String.valueOf(j + 1 );
+            int k= Integer.valueOf(s.substring(0, 1));
+            String number2 = String.valueOf(k + 1 );
+            
+             if(s.contains(number2) && !s.contains("99") )
+            {
+                flag = "YES";
+                ans1 = k;
+                int l =k;
+                String g="";
+                while(g.equalsIgnoreCase(s))
+                {       
+                      if(s.contains(String.valueOf(l)))
+                      {
+                          g+=l;  
+                      }  
+                      else
+                      flag="NO";
+                      l+=1;
+                }
+
+            }
+            else if(s.contains(number))
+            {
+                flag = "YES"; 
+                ans1 = j;
+            }
         }
+        
+        if(flag.equalsIgnoreCase("YES")) 
+        System.out.println(flag + " " +ans1 );
+        else{
+            System.out.println("NO");
+        }
+    }
 }
