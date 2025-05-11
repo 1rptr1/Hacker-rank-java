0d0f4ee0483c6ad0e47edef5385cf45f2f0552d2
Saurav <sauravbhr712@gmail.com>
2025-05-11 20:06:14 +0530

    solution to min max
    Signed-off-by: Saurav <sauravbhr712@gmail.com>

---
+++ src/main/java/minMaxSolution.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class minMaxSolution {
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        int min = arr.get(0);
+        int max = 0;
+        long sum = 0;
+        for (Integer i : arr) {
+            sum += i;
+            if (i > max)
+                max = i;
+            if (i < min)
+                min = i;
+        }
+        System.out.println((sum - max) + " " + (sum - min));
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
