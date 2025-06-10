f9727b788337c7728fce24842841fd7d06fa5ce0
RptR <sauravbhr712@gmail.com>
2025-06-10 22:17:33 +0530

    solution to Sparse array
    Signed-off-by: Saurav <sauravbhr712@gmail.com>

---
+++ src/main/java/week1/sparseArray.java
@@ -0,0 +1,77 @@
+package week1;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+
+public class sparseArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> strings = IntStream.range(0, stringsCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .collect(toList());
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .collect(toList());
+
+
+        List<Integer> res = matchingStrings(strings, queries);
+
+        bufferedWriter.write(
+                res.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+        Map<String, Integer> count = new HashMap<>();
+        for (String s : strings) {
+            count.put(s, count.getOrDefault(s, 0) + 1);
+        }
+        List<Integer> ans = new ArrayList<Integer>();
+        for (String s : queries) {
+            ans.add(count.getOrDefault(s, 0));
+        }
+        return ans;
+    }
+}
